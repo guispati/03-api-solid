bad5011f41b846cdadf8b680fa3a49a9a524a273
Guilherme Spati <guilherme.spati723@gmail.com>
2023-03-29 22:25:34 -0300

    rota de cadastro + testes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client';
+import { ZodError } from "zod";
+import { env } from "./env";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(appRoutes);
 
-prisma.user.create({
-    data: {
-        name: 'Guilherme Spati',
-        email: 'guilherme.spati723@gmail.com'
+app.setErrorHandler((error, _, response) => {
+    if (error instanceof ZodError) {
+        return response.status(400).send({
+            message: 'Validation error', issues: error.format()
+        });
     }
-})
+
+    if (env.NODE_ENV !== 'production') {
+        console.error(error);
+    }
+
+    return response.status(500).send({
+        messaage: 'Internal server error.'
+    });
+});
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,36 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { RegisterUseCase } from "@/use-cases/register";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+
+export async function register(request: FastifyRequest, response: FastifyReply) {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    });
+
+    const { name, email, password } = registerBodySchema.parse(request.body);
+
+    try {
+        const prismaUsersRepository = new PrismaUsersRepository();
+        const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+        await registerUseCase.execute({
+            name,
+            email,
+            password
+        });
+    } catch (err) {
+        if (err instanceof UserAlreadyExistsError) {
+            return response.status(409).send({
+                message: err.message,
+            });
+        }
+
+        throw err;
+    }
+
+    return response.status(201).send();
+};
